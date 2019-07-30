cf7c5abdaf420c829750ba7fe9fc53ea099cdfa8
Rudy <RodriguezLucha@gmail.com>
2019-07-30 16:12:47 -0700

    Add test for DELETE in directories

--- test/directories.js
+++ test/directories.js
@@ -41,13 +41,19 @@ const runCommands = (input) => {
     let [fromParent, fromPointer] = dfs(directories, from.split('/'));
     let [toParent, toPointer] = dfs(directories, to.split('/'));
 
-    console.log(`from: ${from} to: ${to}\n before: ${JSON.stringify(directories, null, 2)}\n`);
-    console.log(`fromPointer: ${JSON.stringify(fromPointer)} fromParent: ${JSON.stringify(fromParent)}`);
-    console.log(`toPointer: ${JSON.stringify(toPointer)} toParent: ${JSON.stringify(toParent)}`);
-    toParent[to.split('/')[to.split('/').length - 1]] = fromParent;
+    // console.log(`from: ${from} to: ${to}\n before: ${JSON.stringify(directories, null, 2)}\n`);
+    // console.log(`fromPointer: ${JSON.stringify(fromPointer)} fromParent: ${JSON.stringify(fromParent)}`);
+    // console.log(`toPointer: ${JSON.stringify(toPointer)} toParent: ${JSON.stringify(toParent)}`);
+    let lastFrom = from.split('/')[from.split('/').length - 1];
+    let lastTo = to.split('/')[to.split('/').length - 1];
 
+    // console.log(`Before: ${JSON.stringify(directories, null, 2)}`);
 
-    console.log(`after: ${JSON.stringify(directories, null, 2)}\n`);
+    let copy = {};
+    copy[lastFrom] = fromPointer;
+    toParent[lastTo] = Object.assign(toParent[lastTo], JSON.parse(JSON.stringify(copy)));
+    delete fromParent[lastFrom];
+    // console.log(`After: ${JSON.stringify(directories, null, 2)}`);
 
     return;
   };
@@ -91,12 +97,12 @@ const runCommands = (input) => {
 describe('Directories', () => {
   it('Echos input', () => {
     let input = `
-  CREATE fruits
-  `;
+CREATE fruits
+`;
 
     let expected = `
-  CREATE fruits
-  `;
+CREATE fruits
+`;
 
     let actual = runCommands(input);
     expect(expected).to.equal(actual);
@@ -109,7 +115,7 @@ CREATE grains
 CREATE fruits/apples
 CREATE fruits/apples/fuji
 LIST
-  `;
+`;
 
     let expected = `
 CREATE fruits
@@ -123,7 +129,7 @@ fruits
     fuji
 grains
 vegetables
-  `;
+`;
 
     let actual = runCommands(input);
     expect(expected).to.equal(actual);
@@ -138,6 +144,64 @@ CREATE fruits/apples/fuji
 LIST
 CREATE grains/squash
 MOVE grains/squash vegetables
+CREATE foods
+MOVE grains foods
+MOVE fruits foods
+MOVE vegetables foods
+LIST
+`;
+
+    let expected = `
+CREATE fruits
+CREATE vegetables
+CREATE grains
+CREATE fruits/apples
+CREATE fruits/apples/fuji
+LIST
+fruits
+  apples
+    fuji
+grains
+vegetables
+CREATE grains/squash
+MOVE grains/squash vegetables
+CREATE foods
+MOVE grains foods
+MOVE fruits foods
+MOVE vegetables foods
+LIST
+foods
+  fruits
+    apples
+      fuji
+  grains
+  vegetables
+    squash
+`;
+
+    let actual = runCommands(input);
+    expect(expected).to.equal(actual);
+  });
+
+
+  it('Handles delete case', () => {
+    let input = `
+CREATE fruits
+CREATE vegetables
+CREATE grains
+CREATE fruits/apples
+CREATE fruits/apples/fuji
+LIST
+CREATE grains/squash
+MOVE grains/squash vegetables
+CREATE foods
+MOVE grains foods
+MOVE fruits foods
+MOVE vegetables foods
+LIST
+DELETE fruits/apples
+DELETE foods/fruits/apples
+LIST
   `;
 
     let expected = `
@@ -154,71 +218,30 @@ grains
 vegetables
 CREATE grains/squash
 MOVE grains/squash vegetables
-  `;
+CREATE foods
+MOVE grains foods
+MOVE fruits foods
+MOVE vegetables foods
+LIST
+foods
+  fruits
+    apples
+      fuji
+  grains
+  vegetables
+    squash
+DELETE fruits/apples
+Cannot delete fruits/apples - fruits does not exist
+DELETE foods/fruits/apples
+LIST
+foods
+  fruits
+  grains
+  vegetables
+    squash
+`;
 
     let actual = runCommands(input);
-    expect(expected).to.equal(actual);
+    expect(expected).to.deep.equal(actual);
   });
-
-
-  // it('Handles full case', () => {
-  //   let input = `
-  // CREATE fruits
-  // CREATE vegetables
-  // CREATE grains
-  // CREATE fruits/apples
-  // CREATE fruits/apples/fuji
-  // LIST
-  // CREATE grains/squash
-  // MOVE grains/squash vegetables
-  // CREATE foods
-  // MOVE grains foods
-  // MOVE fruits foods
-  // MOVE vegetables foods
-  // LIST
-  // DELETE fruits/apples
-  // DELETE foods/fruits/apples
-  // LIST
-  // `;
-
-  //   let expected = `
-  // CREATE fruits
-  // CREATE vegetables
-  // CREATE grains
-  // CREATE fruits/apples
-  // CREATE fruits/apples/fuji
-  // LIST
-  // fruits
-  //   apples
-  //     fuji
-  // grains
-  // vegetables
-  // CREATE grains/squash
-  // MOVE grains/squash vegetables
-  // CREATE foods
-  // MOVE grains foods
-  // MOVE fruits foods
-  // MOVE vegetables foods
-  // LIST
-  // foods
-  //   fruits
-  //     apples
-  //       fuji
-  //   grains
-  //   vegetables
-  //     squash
-  // DELETE fruits/apples
-  // Cannot delete fruits/apples - fruits does not exist
-  // DELETE foods/fruits/apples
-  // LIST
-  // foods
-  //   fruits
-  //   grains
-  //   vegetables
-  //     squash
-  //   `;
-
-  //   let actual = commands(input);
-  //   expect(expected).to.deep.equal(actual);
-  // });
 });
