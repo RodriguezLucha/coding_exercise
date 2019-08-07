487a64ca01c9cdb8d8b0fb33128c2e023cb6d143
Rudy Rodriguez Lucha <rodriguezlucha@gmail.com>
2019-08-06 21:38:10 -0700

    Add recursive permutations in perl

--- Perl/t/recursive_permutations.pl
+++ Perl/t/recursive_permutations.pl
@@ -2,16 +2,37 @@ use Test::More;
 
 sub permutations {
     my $str = shift;
-    return [ 'ab', 'ba' ];
+    if ( length($str) <= 1 ) { return [$str] }
+
+    my @results = ();
+    my @arr     = split //sm, $str;
+    for ( my $i = 0 ; $i <= $#arr ; $i++ ) {
+        my $char      = $arr[$i];
+        my @remaining = ( @arr[ 0 .. $i - 1 ], @arr[ $i + 1 .. $#arr ] );
+
+        my $perms = permutations( join '', @remaining );
+
+        for my $p ( @{$perms} ) {
+            push @results, $char . $p;
+        }
+    }
+
+    return \@results;
 }
 
 sub assertPermutations {
-    my ( $str, $expected ) = @_;
-    my $actual = permutations($str);
-    is_deeply( $actual, $expected, $message );
+    my ( $str, $expected, $message ) = @_;
+    my $got = permutations($str);
+    is_deeply( $got, $expected, $message );
 }
 
-# assertPermutations( 'abc', [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ] );
-assertPermutations( 'ab', [ 'ab', 'ba' ], 'two character string' );
+assertPermutations( '',  [''],  'empty string' );
+assertPermutations( 'a', ['a'], 'one character' );
+assertPermutations( 'ab', [ 'ab', 'ba' ], 'two characters' );
+assertPermutations(
+    'abc',
+    [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ],
+    'three characters'
+);
 
 done_testing;
