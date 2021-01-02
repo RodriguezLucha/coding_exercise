049772c887270ae8de6408d0255649515e1c2f1d
Rudy <RodriguezLucha@gmail.com>
2021-01-01 19:01:04 -0800

    Add python version of is validator

---
+++ Python/recursion/test_parenthesis_validator.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+
+def is_opener(char):
+    return char in ["(", "[", "{"]
+
+
+def is_closer(char):
+    return char in [")", "]", "}"]
+
+
+closer_to_opener = {")": "(", "]": "[", "}": "{"}
+
+
+def is_valid(input_str):
+
+    stack = []
+
+    for char in input_str:
+        if is_opener(char):
+            stack.append(char)
+        else:
+            if is_closer(char):
+                last_opener = stack.pop()
+                if last_opener != closer_to_opener[char]:
+                    return False
+
+    return len(stack) == 0
+
+
+def test_valid_short_code():
+    input_str = "(adbs)"
+    assert is_valid(input_str)
+
+
+def test_longer_code():
+    input_str = "([]{[dssdfa]adf})[asdfa]{{adf}()}"
+    assert is_valid(input_str)
+
+
+def test_mismatched_opener_and_closer():
+    input_str = "([sdfs][]}"
+    assert not is_valid(input_str)
